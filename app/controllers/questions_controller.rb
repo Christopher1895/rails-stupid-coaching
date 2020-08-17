752d837fef07caa2daf4953d456bcec5bc552f4f
Christopher Nitsche <christopher.nitsche@gmx.de>
2020-08-17 14:38:56 +0100

    Task 2 done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:message] == 'I am going to work'
+      @answer = 'Great!'
+
+    elsif params[:message].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work'
+
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
